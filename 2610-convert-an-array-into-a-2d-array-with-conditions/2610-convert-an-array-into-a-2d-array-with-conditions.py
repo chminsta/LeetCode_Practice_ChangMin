48bc25137ee73e8aa9731e65b5a5ae79aecc9c13
chminsta <119744076+chminsta@users.noreply.github.com>
2023-04-08 16:52:46 +0900

    Time: 47 ms (96.51%), Space: 13.8 MB (95.58%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        for num in nums:
+            added = False
+            for row in result:
+                if num not in row:
+                    row.append(num)
+                    added = True
+                    break
+            if not added:
+                result.append([num])
+        return result
